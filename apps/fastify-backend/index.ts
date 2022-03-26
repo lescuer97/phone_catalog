a6ecd733a8848313b3bcb8c4f04c103586313c6b
leonardo <leoescuer@protonmail.com>
2022-03-26 14:52:06 +0100

    added common utils and get call

--- apps/fastify-backend/index.ts
+++ apps/fastify-backend/index.ts
@@ -1,9 +1,20 @@
 import fastify from "fastify";
+import { Phone } from "@phone_catalog/common_tooling";
+import data from "./db/contacts.json";
 
 const app = fastify();
 
-app.get("/ping", async (request, reply) => {
-  return "pa ng\n";
+app.get("/phones", async () => {
+  const jsonContact: Phone[] = data;
+
+  return jsonContact;
+});
+
+app.post("/phones", async (request, reply) => {
+  const jsonContact: Phone[] = data;
+  const newPhone: unknown = request?.body;
+
+  return jsonContact;
 });
 
 app.listen(8080, (err, address) => {
