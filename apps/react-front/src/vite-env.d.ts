00d09ab70e99045185ff7e1ba1272612c4b316c3
leonardo <leoescuer@protonmail.com>
2022-03-29 19:54:34 +0200

    updating packages and adding env variables

--- apps/react-front/src/vite-env.d.ts
+++ apps/react-front/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_DEVELOP_SERVER: string;
+  readonly VITE_ENVIROMENT: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
