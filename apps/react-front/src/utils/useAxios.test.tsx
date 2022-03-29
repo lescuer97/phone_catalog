584f778944527b552e657799bb0b3f82cdd25554
leonardo <leoescuer@protonmail.com>
2022-03-29 23:47:58 +0200

    fix top tsx

--- apps/react-front/src/utils/useAxios.test.tsx
+++ apps/react-front/src/utils/useAxios.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks";
 import useAxios from "./useAxios";
 
 test("testing initial setUp", () => {
