26109a4bfcb394be396e5f36ee1e260b9f394eb0
leonardo <leoescuer@protonmail.com>
2022-03-29 22:26:20 +0200

    testing setup

--- apps/react-front/vite.config.ts
+++ apps/react-front/vite.config.ts
@@ -6,7 +6,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxRuntime: "classic" })],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
